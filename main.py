fe15402f682d9364df3e2528e9a473910ca81670
Ciprian I <ivanovciprian03@gmail.com>
2024-09-05 17:10:07 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+
+logo = """ 
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ '.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ '.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   '._____.'  | || ||____|  |____|| || |  |________|  | || |   '._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+
+Author: Ciprian Ivanov
+"""
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+def calculator():
+    print(logo)
+    math_more = True
+    first_number = float(input("Enter a number:  "))
+    while math_more:
+        for key in operations:
+            print(key)
+        operation = input("Choose an operation: ")
+        next_number = float(input("Enter another number:  "))
+        result = operations[operation](first_number, next_number)
+        print(f"{first_number} {operation} {next_number} = {result}")
+        first_number = result
+        decision = input("Continue? Type \"y\" to continue or \"n\" to start again:  ").lower()
+        if decision != "y":
+            math_more = False
+            os.system("cls")
+            calculator()
+            
+calculator()
