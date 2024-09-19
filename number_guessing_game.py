cb2aab0981e087d68ee71785d496f33132871145
Ciprian I <ivanovciprian03@gmail.com>
2024-09-19 14:32:27 +0100

    01_guessing_game.py
    Updated and refactored number guessing game

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -12,6 +12,10 @@ import os
 from time import sleep
 
 
+EASY_GAME = 10
+HARD_GAME = 5
+
+
 def low_high(n1, n2):
     """Takes 2 numbers and checks which one is bigger or smaller 
     and returns 1"""
@@ -22,13 +26,27 @@ def low_high(n1, n2):
     return 1
    
 
+def difficulty():
+    """Sets the difficulty of the game , 5 of 10 turns"""
+    difficulty_choice = input("Choose a difficulty. Type \"easy\" or \"hard\": ")
+    if difficulty_choice == "easy":
+        return EASY_GAME
+    else:
+        return HARD_GAME
+    
+def end_game(user_input, random_no, tries):
+    if tries == 0:
+        print("You lose!")
+        return False 
+    if user_input == random_no:
+        print("You win!")
+        return False 
+
 
 def number_guessing():
     """Number Guessing Game, guess a number between 1 and 100.
     The game has two diffcuilties: easy and hard"""
     continue_game = True 
-    easy_game = 10
-    hard_game = 5
     tries = 0
     random_no = random.choice(range(1, 101))
     
@@ -41,34 +59,27 @@ def number_guessing():
     sleep(0.8)
 
    
-    difficulty_choice = input("Choose a difficulty. Type \"easy\" or \"hard\": ")
-    if difficulty_choice == "easy":
-        tries = easy_game
-    else:
-        tries = hard_game
+    tries = difficulty()
    
     while continue_game:
         print(f"You have {tries} tries left.")
         user_input = int(input("Make a choice: "))
             
-        tries -= int(low_high(user_input, 
-                              random_no,))
-        if user_input == random_no:
-           print("You win!")
-           continue_game = False
-        if tries == 0 and player != random_no:
-           print("You lose!")
-           continue_game =  False   
+        tries -= int(low_high(user_input, random_no,))
+        print(f"Tries: {tries}")
+        continue_game = end_game(user_input, random_no, tries)
+        
+        # if tries == 0:
+        #    print("You lose!")
+        #    continue_game =  False 
+        # if user_input == random_no:
+        #    print("You win!")
+        #    continue_game = False 
     choice = input("Wanna play again? \"yes\" or \"no\"  ")
     if choice == "yes":
-        os.system('cls')
+        os.system('clear')
         number_guessing()
     
     
 number_guessing()
 
-
-
-
-
-      
